34c274c34f85dcd9136a23f9beab27462352b412
Dagruun <evante2007@gmail.com>
2025-08-28 13:03:28 -0500

    added major to the output

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Evan Teeters
+ * Date: 2025/08/28
+ *
+ *Prints name and major.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Evan Teeters!\n");
+    printf("Electrical Engineering\n");
+
+    return 0;
+}
